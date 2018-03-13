adcbacb9f49c1699d8bfb232b770b16153c0bfd8
Eden Mesfun <emesfun@gmail.com>
2018-03-13 14:21:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,8 +43,9 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, SUM(pledges.amount)
-FROM projects 
+FROM projects
 INNER JOIN pledges
+ON
 ;"
 end
 
