4bae2d13b7782ae49132f2a78ea552ed7e35bfb6
Eden Mesfun <emesfun@gmail.com>
2018-03-13 13:18:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,6 +29,8 @@ FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.funding_goal;"
+
+WHERE SUM(pledges.amount) >= projects.funding_goal
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
